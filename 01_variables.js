15cb338ff182116d15dff1236b42a143cc4e89dc
acynonix <user@company.com>
2024-07-14 01:54:31 +0530

    learned primitive datatypes

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 123123
+let accountEmail = "abc@company.com"
+var accountPass = "123123123"
+accountCity = "Area00"
+
+// accountId = 21      // not allowed to change the constant variables
+// console.log(accountId);
+
+accountEmail = "zxc@comp.com"
+accountPass = "34634"
+accountCity = "NYC"
+
+console.table([accountId, accountEmail, accountPass, accountCity])
---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+// number (has some range limit)
+let phNo = 123123123;
+
+//bigInt
+
+
+// string
+let name = "Eleven";
+
+// boolean
+let isLoggedIn = true;
+
+// null (standalone value, object type)
+let temperature = null;
+
+// undefined
+let myHeight;
+
+
+// symbol
+
+
+console.log(typeof temperature);
+console.table([phNo, name, isLoggedIn, temperature, myHeight]);
