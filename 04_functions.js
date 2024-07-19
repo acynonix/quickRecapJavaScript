ffef76c48c0c648b6a15eab31fd618ab15be5a86
acynonix <user@company.com>
2024-07-20 01:44:36 +0530

    add functions.js file

---
+++ 04_functions.js
@@ -0,0 +1,14 @@
+// functions in Javascript
+
+function myFunc(){
+	console.log("Hello World!");
+}
+
+function mySecondFunc(num1, num2){
+	return num1+num2;
+}
+
+myFunc();
+
+console.log(mySecondFunc(10, 20));
+
